4c08791e2c774b4dd8adb7d90706ba26cc36afa3
Madeline <madelign@gmail.com>
2020-03-01 23:38:57 -0800

    grandpa second version

---
+++ ltp/ch6grandma2.rb
@@ -0,0 +1,16 @@
+puts grandchild_response = "Hi grandma"
+
+while grandchild_response != "BYE"
+grandchild_response = gets.chomp.to_s
+
+while grandchild_response == grandchild_response.downcase 
+puts "HUH?!  SPEAK UP, SONNY!"
+
+year = (rand(1930..1951))
+
+grandchild_response == 
+grandchild_response.upcase
+  puts "NO, NOT SINCE #{year}!"
+end 
+
+end 
