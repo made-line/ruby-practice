5c84a29f45d5f5541e46248763534f9ed2c2f2b3
Madeline <madelign@gmail.com>
2020-03-11 23:41:40 -0700

    notes and small revision

--- ltp/ch7bubblesort.rb
+++ ltp/ch7bubblesort.rb
@@ -4,14 +4,22 @@ list = []
 
 words = 0
 
+# loop until user inputs enter/empty string
 until words == ""
   words = gets.chomp 
   list << words
 end 
 
+# iterate over first two elements, compare using > or <, switch elements accordingly. 
+#repeat until entire array is sorted
+
+
+
 n = list.length
 i = 0
 while i < n
-  i += 1
   print list[i]
+  i += 1
 end 
+
+# have final loop go through entire array to make sure there aren't any swaps - sort is finished 
