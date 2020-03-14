5b41bff76a4497a7322d0f417feb6de3204472e4
Madeline <madelign@gmail.com>
2020-03-14 00:40:49 -0700

    finished leap year

--- ltp/ch6leapyears.rb
+++ ltp/ch6leapyears.rb
@@ -5,24 +5,18 @@ starting_year = gets.chomp
 puts 'Enter an ending year:'
 ending_year = gets.chomp
 
-# starting year needs to be less than or equal to ending year
-if starting_year <= ending_year
-elsif starting_year > ending_year
-    puts "oh no!"
-
 # leap years are divisible by 4
 # years divisible by 100 are not leap years UNLESS they're divisible by 400
 # 1600 & 2000 were leap years
 
-if starting_year.to_i % 400 == 0
-puts starting_year
-  elsif starting_year.to_i % 100 == 0
-  elsif starting_year.to_i % 4 == 0
-puts starting_year
-end
+# iterate through range with current_year variable while satisfying leap year conditions
+leap_years = (starting_year.to_i..ending_year.to_i).select do |current_year|
+    if current_year.to_i % 400 == 0
 
-
-
-end
+    elsif current_year.to_i % 100 == 0
+    elsif current_year.to_i % 4 == 0
+      puts current_year
+    end
+  end
 
  
