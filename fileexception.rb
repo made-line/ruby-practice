c9ae2fa4eee8cec766b3a849174b32145a67440e
Madeline <madelign@gmail.com>
2020-03-19 23:46:25 -0700

    file exception practice

---
+++ fileexception.rb
@@ -0,0 +1,18 @@
+File.foreach( 'does_not_exist.txt' ) do |line|
+	puts line.chomp 
+end 
+
+# rescue --> catch the exception that's being thrown
+# exception gets mapped to e variable
+rescue Exception => e
+	puts e.message
+	puts "Woopsy daisy"
+end
+
+
+# alternative to exception
+if File.exist? 'does_not_exist.txt'
+	File.foreach( 'does_not_exist.txt' ) do |line|
+		puts line.chomp
+	end
+end
